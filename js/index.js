564b89c9d38a48c6b2f1c7917c98d7e6fd054078
Adilmar Dantas <akanehar@gmail.com>
2018-10-22 15:30:51 -0300

    app vempraufu

--- js/index.js
+++ js/index.js
@@ -1,3 +1,20 @@
+// Add to index.js or the first page that loads with your app.
+// For Intel XDK and please add this to your app.js.
+
+document.addEventListener('deviceready', function () {
+  // Enable to debug issues.
+  // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+  
+  var notificationOpenedCallback = function(jsonData) {
+    console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+  };
+
+  window.plugins.OneSignal
+    .startInit("40314c41-8761-4af9-b88a-049e7ca5c8ff")
+    .handleNotificationOpened(notificationOpenedCallback)
+    .endInit();
+}, false);
+
 /**
  * Modified MIT License
  *
